cf4114023c8aa8f1354a78183150fe0a70013a30
Egorov Yuri <egorov@4int.ru>
2014-04-12 23:07:31 +0400

    upd setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 cwd=`pwd`
 
-ln -sfi ${cwd}/.i3 ${cwd}/.xprofile ${cwd}/.Xresources ${cwd}/.inputrc ${cwd}/.bashrc ${cwd}/.bash_profile ${cwd}/.pylintrc ${cwd}/.hgrc ${HOME}
+ln -sfi ${cwd}/.i3 ${cwd}/.xprofile ${cwd}/.Xresources ${cwd}/.inputrc ${cwd}/.bashrc ${cwd}/.bash_profile ${cwd}/.pylintrc ${cwd}/.hgrc ${cwd}/.bash_aliases ${cwd}/.bash_functions ${cwd}/.dircolors ${cwd}/.gitconfig ${HOME}
