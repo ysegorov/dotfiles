53b3c92a2ed684e1c813840a17b41b25fc9beaca
Egorov Yuri <egorov@4int.ru>
2014-04-13 16:13:41 +0400

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 
 cwd=`pwd`
 
-ln -sfi ${cwd}/.i3 ${cwd}/.xprofile ${cwd}/.Xresources ${cwd}/.inputrc ${cwd}/.bashrc ${cwd}/.bash_profile ${cwd}/.pylintrc ${cwd}/.hgrc ${cwd}/.bash_aliases ${cwd}/.bash_functions ${cwd}/.dircolors ${cwd}/.gitconfig ${HOME}
+ln -sfiv ${cwd}/.i3 ${cwd}/.xprofile ${cwd}/.Xresources ${cwd}/.inputrc ${cwd}/.bashrc ${cwd}/.bash_profile ${cwd}/.pylintrc ${cwd}/.hgrc ${cwd}/.bash_aliases ${cwd}/.bash_functions ${cwd}/.dircolors ${cwd}/.gitconfig ${HOME}
+
+mkdir -p ${HOME}/.config/
+ln -sfiv ${cwd}/.config/dunst ${HOME}/.config/
