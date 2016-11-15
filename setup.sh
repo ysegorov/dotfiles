5deee62ef0d670378d30be37bd313b0c61334619
Yuri Egorov <ysegorov@gmail.com>
2016-11-15 13:57:35 +0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ cwd=`pwd`
 
 ln -sfi ${cwd}/bin ${HOME}/
 ln -sfi ${cwd}/.bash.d ${HOME}/
-ln -sfiv ${cwd}/.i3 ${cwd}/.xprofile ${cwd}/.Xresources ${cwd}/.inputrc ${cwd}/.bashrc ${cwd}/.bash_profile ${cwd}/.pylintrc ${cwd}/.hgrc ${cwd}/.dircolors ${cwd}/.gitconfig ${cwd}/.npmrc ${cwd}/.psqlrc ${HOME}
+ln -sfiv ${cwd}/.i3 ${cwd}/.xprofile ${cwd}/.Xresources ${cwd}/.inputrc ${cwd}/.bashrc ${cwd}/.bash_profile ${cwd}/.pylintrc ${cwd}/.hgrc ${cwd}/.dircolors ${cwd}/.gitconfig ${cwd}/.npmrc ${cwd}/.ackrc ${cwd}/.psqlrc ${HOME}
 
 mkdir -p ${HOME}/.config/
 ln -sfiv ${cwd}/.config/dunst ${HOME}/.config/
