f86c9d77b57137885481d9be929b1a22b06aae69
Yuri Egorov <ysegorov@gmail.com>
2021-02-06 14:43:55 +0300

    move .pylintrc to .config/pylint/config

--- .local/env.d/50-progs.bash
+++ .local/env.d/50-progs.bash
@@ -1,2 +1,4 @@
 export INPUTRC=${XDG_CONFIG_HOME}/inputrc/inputrc
 export MBSYNC_CONFIG=${XDG_CONFIG_HOME}/mbsync/config
+export PYLINTHOME=${XDG_CACHE_HOME}/pylint
+export PYLINTRC=${XDG_CONFIG_HOME}/pylint/config
