98eecd55d93209860731caaa003ef98e8c4fac08
Yuri Egorov <ysegorov@gmail.com>
2021-02-06 14:29:49 +0300

    update mbsync config, move INPUTRC env var to another file

--- .local/env.d/01-bash.bash
+++ .local/env.d/01-bash.bash
@@ -1,2 +1,1 @@
 export BASH_D_DIR=${HOME}/.local/bash.d
-export INPUTRC=${HOME}/.config/inputrc/inputrc
---
+++ .local/env.d/50-progs.bash
@@ -0,0 +1,2 @@
+export INPUTRC=${XDG_CONFIG_HOME}/inputrc/inputrc
+export MBSYNC_CONFIG=${XDG_CONFIG_HOME}/mbsync/config
