ad0f1d944c83c35f8cb8acf4bf4922ed717aabde
Yuri Egorov <ysegorov@gmail.com>
2021-02-06 14:54:19 +0300

    move .psqlrc to .config/psql/config

--- .local/env.d/50-progs.bash
+++ .local/env.d/50-progs.bash
@@ -1,4 +1,7 @@
 export INPUTRC=${XDG_CONFIG_HOME}/inputrc/inputrc
 export MBSYNC_CONFIG=${XDG_CONFIG_HOME}/mbsync/config
+
 export PYLINTHOME=${XDG_CACHE_HOME}/pylint
 export PYLINTRC=${XDG_CONFIG_HOME}/pylint/config
+
+export PSQLRC=${XDG_CONFIG_HOME}/psql/config
